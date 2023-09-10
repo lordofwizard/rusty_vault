79aa8d0bedddac1b530b4999abf7bde75dc8de26
lordofwizard <advaitpandharpurkar619@gmail.com>
2023-09-11 00:14:52 +0530

    Added hashmap in rust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 mod run;
 mod pass_entry;
 
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Entry(String, String);
+
 fn main(){
+
+    let mut passwords : HashMap<String , Entry> = HashMap::new();
+    passwords.insert(String::from("Google"),Entry(String::from("advait@gmail.com"),String::from("iamyofathr")));
+
+
+    println!("{:?}",passwords);
     println!("Hello World, babyyy!");
+
 }
 
