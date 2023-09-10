ea3ea732437419543c1fccc52fa0c57695a6c731
lordofwizard <advaitpandharpurkar619@gmail.com>
2023-09-10 23:46:14 +0530

    starting from scratch again

---
+++ src/pass_entry.rs
@@ -0,0 +1,19 @@
+pub struct PasswordEntry {
+    pub username: String,
+    pub password: String,
+    pub website: String,
+}
+
+impl PasswordEntry {
+    pub fn new(username: &str, password: &str, website: &str) -> PasswordEntry {
+        PasswordEntry {
+            username: username.to_string(),
+            password: password.to_string(),
+            website: website.to_string(),
+        }
+    }
+    pub fn to_string(&self) -> String {
+        format!("Username: {}\nPassword: {}\nWebsite: {}\n", self.username, self.password, self.website)
+    }
+}
+
