66648725ec35709389ab7c74342fd0bc5d9f6741
lordofwizard <advaitpandharpurkar619@gmail.com>
2023-09-18 11:15:33 +0530

    working version with searching

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,37 @@
-mod pass_entry;
 mod pentry;
-mod run;
 
 use crate::pentry::prompt;
 use crate::pentry::read_passwords_from_file;
 use crate::pentry::ServiceInfo;
 
+fn clr(){
+    print!("{}[2J", 27 as char);
+}
 fn main() {
-    let services = read_passwords_from_file().unwrap_or_else(|err| {
-        eprintln!("Error reading passwords: {}", err);
-        Vec::new()
-    });
-
+    clr();
+println!("              _                                       _                                 "); 
+println!("            /' `\\                      /'            ' )       )                   /'  /'");
+println!("          /'     )                 --/'--             /      _/                  /'--/'--");
+println!("        /' (___,/'        ____     /'                /    _/~____              /'  /'   "); 
+println!("      /'   ;   /'    /  /'    )--/' /'    /         /  _/~ /'    )  /'    /  /'  /'     "); 
+println!("    /'    /' /'    /'  '---,   /' /'    /'         /_/~  /'    /' /'    /' /'  /'       "); 
+println!("(,/'     (_,(___,/(__(___,/   (__(___,/(__        /~    (___,/(__(___,/(__(__ (__       "); 
+println!("                                    /'                                                  "); 
+println!("                            /     /'                                                    "); 
+println!("                           (___,/'                                                      "); 
     loop {
         println!("Password Manager Menu:");
         println!("1. Add Entry");
         println!("2. List Entries");
-        println!("3. Quit");
+        println!("3. Search");
+        println!("4. Quit");
 
         let mut choice = String::new();
         std::io::stdin().read_line(&mut choice).unwrap();
 
         match choice.trim() {
             "1" => {
+                clr();
                 let entry = ServiceInfo::new(
                     prompt("Service :"),
                     prompt("Username :"),
@@ -32,6 +41,7 @@ fn main() {
                 entry.write_to_file();
             }
             "2" => {
+                clr();
                 let services = read_passwords_from_file().unwrap_or_else(|err| {
                     eprintln!("Error reading passwords: {}", err);
                     Vec::new()
@@ -43,14 +53,28 @@ fn main() {
                 }
             }
             "3" => {
+                clr();
+                let services = read_passwords_from_file().unwrap_or_else(|err| {
+                    eprintln!("Error reading passwords: {}", err);
+                    Vec::new()
+                });
+                let search = prompt("Search :");
+                for item in &services {
+                    if item.service.as_str() == search.as_str() {
+                        
+                    println!("Service = {}
+    - Username : {} 
+    - Password : {}", item.service, item.username, item.password);
+                    }
+                }
+            }
+            "4" => {
+                clr();
                 println!("Goodbye!");
                 break;
             }
             _ => println!("Invalid choice."),
         }
+        println!("\n\n");
     }
-    for service in &services {
-        println!("{:?}", service);
-    }
-    //_my_service.write_to_file();
 }
