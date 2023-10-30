9c90c1b8cc3c2bcd26fa54b400094e96d19f8150
FatGuyy <omkarjadhav2103@gmail.com>
2023-10-31 00:05:14 +0530

    Add the comments

--- src/db.rs
+++ src/db.rs
@@ -1,16 +1,56 @@
+use std::io;
+use std::io::Write;
 extern crate rusqlite;
-use rusqlite::Connection;
-use crate::pentry::ServiceInfo;
+use rusqlite::{Connection, Error};
+use serde::{Deserialize, Serialize};
 
-pub fn init_database() -> Result<Connection, rusqlite::Error> {
-    let conn = Connection::open("passwords.db")?; // You can change the database filename
+
+// This code defines the a struct ServiceInfo 
+// for storing all the data of what is to be stored in the database.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ServiceInfo {
+    pub id: Option<i64>,
+    pub service: String,
+    pub username: String,
+    pub password: String,
+}
+
+// Constructor to initialize new struct ServiceInfo
+impl ServiceInfo {
+    pub fn new(service: String, username: String, password: String) -> Self {
+        ServiceInfo {
+            id: None,
+            service,
+            username,
+            password,
+        }
+    }
+}
+
+// Prompt function takes in a string as argument 
+// and Displays it while also taking in one input and returns the input
+pub fn prompt(prompt: &str) -> String {
+    print!("{}", prompt);
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    input.trim().to_string()
+}
+
+// function to initialize the database
+//      Creates the database if it does not exist 
+//      Creates a table 'passwords' if it does not exist with column matching with the Struct ServiceInfo
+pub fn init_database() -> Result<Connection, Error> {
+    let conn = Connection::open("passwords.db")?;
 
     conn.execute(
         "CREATE TABLE IF NOT EXISTS passwords (
             id INTEGER PRIMARY KEY,
             service TEXT,
             username TEXT,
-            encrypted_password TEXT
+            password TEXT
         )",
         [],
     )?;
@@ -18,36 +58,45 @@ pub fn init_database() -> Result<Connection, rusqlite::Error> {
     Ok(conn)
 }
 
-pub fn write_password_to_db(
+// This function is to write the Data to the password table
+// Takes 2 main input as parameters
+    // 1. Connection variable to the database
+    // 2. All the parameters to put inside database
+// Then inserts them into the database with INSERT command and the input as parameters
+    pub fn write_password_to_db(
     conn: &Connection,
     service: &str,
     username: &str,
-    encrypted_password: &str,
-) -> Result<(), rusqlite::Error> {
+    password: &str,
+) -> Result<(), Error> {
     conn.execute(
-        "INSERT INTO passwords (service, username, encrypted_password) VALUES (?, ?, ?)",
-        &[&service, &username, &encrypted_password],
+        "INSERT INTO passwords (service, username, password) VALUES (?, ?, ?)",
+        &[&service, &username, &password],
     )?;
     Ok(())
 }
 
-pub fn read_passwords_from_db(conn: &Connection) -> Result<Vec<ServiceInfo>, rusqlite::Error> {
-    let mut stmt = conn.prepare("SELECT service, username, encrypted_password FROM passwords")?;
+// This function reads all the data from the passwords table and returns all the ENTRIES from the table.
+pub fn read_passwords_from_db(conn: &Connection) -> Result<Vec<ServiceInfo>, Error> {
+    let mut stmt = conn.prepare("SELECT service, username, password FROM passwords")?;
     let entries = stmt
         .query_map([], |row| {
             Ok(ServiceInfo::new(
                 row.get(0)?,
                 row.get(1)?,
                 row.get(2)?,
-                // &encryption_key,
             ))
         })?
         .collect::<Result<Vec<_>, _>>()?;
     Ok(entries)
 }
 
-pub fn search_service_by_name(conn: &Connection, name: &str) -> Result<Option<ServiceInfo>, rusqlite::Error> {
-    let mut stmt = conn.prepare("SELECT id, service, username, encrypted_password FROM passwords WHERE service = ?")?;
+// This function searches for a service entry by name in the database
+    // Statement to connect to database
+    // Result is used to query a row from the passwords table and pass in the service name as parameters
+    // Return the row after checking errors and no query returns  
+pub fn search_service_by_name(conn: &Connection, name: &str) -> Result<Option<ServiceInfo>, Error> {
+    let mut stmt = conn.prepare("SELECT id, service, username, password FROM passwords WHERE service = ?")?;
     let result = stmt.query_row(&[name], |row| {
         Ok(ServiceInfo {
             id: Some(row.get(0)?),
@@ -59,7 +108,7 @@ pub fn search_service_by_name(conn: &Connection, name: &str) -> Result<Option<Se
 
     match result {
         Ok(entry) => Ok(Some(entry)),
-        Err(rusqlite::Error::QueryReturnedNoRows) => Ok(None),
+        Err(Error::QueryReturnedNoRows) => Ok(None),
         Err(err) => Err(err),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
-mod pentry;
-use crate::pentry::prompt;
-// use crate::pentry::read_passwords_from_file;
-use crate::pentry::ServiceInfo;
-
+// If you get a error at first time running this project - Install libsqlite3-dev and sqlite3
+// sudo apt-get install sqlite3 libsqlite3-dev
 mod db;
-use db::{init_database, write_password_to_db, read_passwords_from_db, search_service_by_name};
+use db::*;
 
+// function named clr which clears the terminal screen
+// It uses the [2J command which tells the terminal to clear the screen
 fn clr() {
     print!("{}[2J", 27 as char);
 }
+
+// This are first few lines of the code, 
+// it imports the db.rs file,
 fn main() {
+    // Making connection to the database
     let conn = init_database().expect("Failed to initialize the database");
     clr();
+    // Here, the main function begins, first thing, it begins the connection to the database,
+    // then it defines the asciii art and prints it.
     let ascii = r#"
 
     ________  ________  ________   ________           ___      ___ ________  ___  ___  ___   _________   
@@ -27,7 +32,11 @@ fn main() {
 
     "#;
     println!("{ascii}");
+
+    // This is the main loop
     loop {
+        // This prints the Menu of the app
+        // takes input from user in a variable named choice to then decide which action to do.
         println!("Password Manager Menu:");
         println!("1. Add Entry");
         println!("2. List Entries");
@@ -37,6 +46,9 @@ fn main() {
         let mut choice = String::new();
         std::io::stdin().read_line(&mut choice).unwrap();
 
+        // Here we use match to work according to the input
+        // It clears the terminal first and then,
+            // If the input is “1”, we get the input from user and write it to the database
         match choice.trim() {
             "1" => {
                 clr();
@@ -45,7 +57,6 @@ fn main() {
                     prompt("Username :"),
                     prompt("Password :"),
                 );
-                println!("Entry added successfully.");
                 write_password_to_db(
                     &conn,
                     &entry.service,
@@ -53,14 +64,16 @@ fn main() {
                     &entry.password,
                 )
                 .expect("Failed to write to the database");
+                println!("Entry added successfully.");
     
             }
+            // If choice is 2, clears the terminal first and then
+                // This lets the user to list the stored service entries
+                // And display all of them on the terminal using a for loop
             "2" => {
                 clr();
                 let services = read_passwords_from_db(&conn).unwrap_or_else(|err| {
                     eprintln!("Error reading passwords: {}", err);    
-                // let services = read_passwords_from_file().unwrap_or_else(|err| {
-                    // eprintln!("Error reading passwords: {}", err);
                     Vec::new()
                 });
                 for item in &services {
@@ -72,6 +85,10 @@ fn main() {
                     );
                 }
             }
+            // This function lets the user search for the password and username by its service name
+                // 1. Clear the terminal
+                // 2. Uses a match on the function search_service_by_name to handle the various returns it can give
+                // 3. If the query is found it prints the data, if it is not found it prints service not found, else prints the error thrown.
             "3" =>{
                 clr();
                 let search = prompt("Search by service name:");
@@ -92,13 +109,15 @@ fn main() {
                     }
                 }
             }
-            
+
+            // If the choice is “4”, it prints “Goodbye” and breaks out of the loop
+            // And handles the error if the choice is not valid
             "4" => {
                 clr();
                 println!("Goodbye!");
                 break;
             }
-            _ => println!("Invalid choice."),
+            _ => println!("Invalid choice."), // Handles all the inputs other than 1, 2, 3, 4 
         }
         println!("\n\n");
     }
